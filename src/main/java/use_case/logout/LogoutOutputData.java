ff9b43c0bead7ade05fe376a4922ed3efe47f282
Barış <bayazitbaris@gmail.com>
2024-10-21 09:53:52 -0400

    Try complete LogoutInteractor LogoutOutputData LogoutInputData

--- src/main/java/use_case/logout/LogoutOutputData.java
+++ src/main/java/use_case/logout/LogoutOutputData.java
@@ -4,12 +4,12 @@ package use_case.logout;
  * Output Data for the Logout Use Case.
  */
 public class LogoutOutputData {
-
-    private String username;
-    private boolean useCaseFailed;
+    private final String username;
+    private final boolean useCaseFailed;
 
     public LogoutOutputData(String username, boolean useCaseFailed) {
-        // TODO: save the parameters in the instance variables.
+        this.username = username;
+        this.useCaseFailed = useCaseFailed;
     }
 
     public String getUsername() {
